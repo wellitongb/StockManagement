2e9c07ff8b8becc8f01d89211cd15e8fc572dc69
wellitongb <wellitongb@A205-11B.imd.local>
2019-05-30 12:55:47 -0300

    Criação de algumas validações especificas da aplicação!

--- main/service/ValidacaoSalario.java
+++ main/service/ValidacaoSalario.java
@@ -8,13 +8,13 @@ public class ValidacaoSalario implements ValidacaoUsuario{
 
 	@Override
 	public String validacao(Usuario usuario, String param, String key) throws ServiceException {
-		if ( !usuario.getClass().equals(Funcionario.class) ) 
+		if( !usuario.getClass().equals(Funcionario.class) ) 
 			throw new ServiceException("Tipo de usuario invalido!");
 		
-		if(!param.equals("salario"))
+		if( !param.equals("salario") )
 			throw new ServiceException("Parametro invalido!");
 			
-		if( Integer.parseInt(key) >= 0 ) 
+		if( !(Integer.parseInt(key) >= 0)) 
 	            throw new ServiceException("Quantidade inválida de salario!");
 	     
 		return "OK";
