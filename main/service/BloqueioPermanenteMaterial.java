df2ad9a9b2ef5c854098dfc9363084e52fad7c5f
samuellucas97 <samuellucas97@A205-16.imd.local>
2019-05-30 12:27:13 -0300

    atualizando repositório.

--- main/service/BloqueioPermanenteMaterial.java
+++ main/service/BloqueioPermanenteMaterial.java
@@ -3,13 +3,15 @@ package service;
 import model.Material;
 import model.StatusSM;
 import dao.IMaterialDAO;
+import exception.ServiceException;
 
 public class BloqueioPermanenteMaterial extends BloqueioMaterial {
 
 	/**
+	 * @throws ServiceException 
 	 * @see service.BloqueioMaterial#bloquear(Model.Material, dao.IMaterialDAO, Model.String)
 	 */
-	public void bloquear(Material material, IMaterialDAO materialDAO, String causa) {
+	public void bloquear(Material material, IMaterialDAO materialDAO, String causa) throws ServiceException {
 		material.setStatus( StatusSM.Permanente );
 		material.setCausa( causa );
 		materialDAO.alterar( material );
