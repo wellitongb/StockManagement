cf8322ef87994bbf48966a5c86b16920eead91e5
wellitongb <wellitongb@A204-17L.imd.local>
2019-06-08 15:24:38 -0300

    Organizando pacote validação.

--- main/service/validacao/ValidacaoMaterial.java
+++ main/service/validacao/ValidacaoMaterial.java
@@ -8,37 +8,43 @@ public class ValidacaoMaterial {
 
 	/// MÉTODOS
 
-	public String validacao(Material material) throws ServiceException{
-            
-            if(material.getIdMaterial() < 0){
-                throw new ServiceException("Material criado com ID errado!");
-            }
-            
-            if((material.getStatus() == StatusSM.NaoBloqueado && 
-                    !material.getCausa().equals(" "))
-                    ||
-                    (material.getStatus() != StatusSM.NaoBloqueado &&
-                    material.getCausa().equals(" "))){
-                throw new 
-                ServiceException("Material possui status ou causa indevida!");
-            
-            }
-            
-            if(material.getDataEntrada() == null){
-             throw new ServiceException("Material criado sem data de entrada!");
-            }
-            
-            if(material.getQuantidade() < 0){
-                throw new 
-                ServiceException("Material criado com quantidade inválida!");
-            }
-            
-            if(material.getValorUnitario() < 0){
-                throw new 
-                ServiceException("Material criado com valor unitário inválido!");
-            }
-            
-            return "OK";
+    /**
+     *
+     * @param material
+     * @return
+     * @throws ServiceException
+     */
+    public String validacao(Material material) throws ServiceException{
+
+        if(material.getId() < 0){
+            throw new ServiceException("Material criado com ID errado!");
         }
+
+        if((material.getStatus() == StatusSM.NaoBloqueado && 
+                !material.getCausa().equals(" "))
+                ||
+                (material.getStatus() != StatusSM.NaoBloqueado &&
+                material.getCausa().equals(" "))){
+            throw new 
+            ServiceException("Material possui status ou causa indevida!");
+
+        }
+
+        if(material.getDataEntrada() == null){
+         throw new ServiceException("Material criado sem data de entrada!");
+        }
+
+        if(material.getQuantidade() < 0){
+            throw new 
+            ServiceException("Material criado com quantidade inválida!");
+        }
+
+        if(material.getValorUnitario() < 0){
+            throw new 
+            ServiceException("Material criado com valor unitário inválido!");
+        }
+
+        return "OK";
+    }
 
 }
